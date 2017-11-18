9d0b54c893b37489dae18a2d94135b24d278b564
Achille AROUKO <achille.arouko@gmail.com>
2017-11-18 16:26:34 +0100

    setup final

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'wallet-python',
+    packages = ['wallet-python'],
+    version = 'v1.0-beta',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Wallet NG, python library',
+    author = 'Achille AROUKO',
+    author_email = 'achille.arouko@gmail.com',
+    url = 'https://github.com/alekaizer/wallet-python',
+    download_url = 'https://github.com/alekaizer/wallet-python/archive/v1.0-beta.tar.gz',
+    keywords = ['payment', 'wallet.ng', 'wallet-python', 'python-wallet', 'wallet-ng-python'],
+    classifiers = [],
+)
